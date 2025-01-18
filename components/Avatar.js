a0b84566b5c38387550bff11f1a77a70ade62e93
TURANSKY\turan <turan.zoltan0614@gmail.com>
2025-01-18 16:32:44 +0100

    Post Form

---
+++ components/Avatar.js
@@ -0,0 +1,7 @@
+export default function Avatar() {
+    return (
+        <div className='w-12 rounded-full overflow-hidden'>
+            <img src='https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?q=80&w=1966&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Card from "../../components/Card";
 import NavigationCard from "../../components/NavigationCard";
+import PostFormCard from "../../components/PostFormCard";
+
 
 export default function Home() {
   return (
     <div className="flex max-w-7xl mx-auto gap-5">
       <NavigationCard />
       <div className="grow">
-        <Card>form here</Card>
+        <PostFormCard></PostFormCard>
         <Card>first post test</Card>
       </div>
     </div>
