99b6a7608a0581da39dc8b681fdc63770df057da
TURANSKY\turan <turan.zoltan0614@gmail.com>
2025-01-18 01:57:20 +0100

    Navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import Card from "../../components/Card";
+import NavigationCard from "../../components/NavigationCard";
 
 export default function Home() {
   return (
     <div className="flex max-w-7xl mx-auto gap-5">
-      <div className="w-1/3">
-        <Card>test link</Card>
-      </div>
+      <NavigationCard />
       <div className="grow">
         <Card>form here</Card>
         <Card>first post test</Card>
