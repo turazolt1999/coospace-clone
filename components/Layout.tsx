75cc72528bce1b69f15d7e06319375fb660f0957
TURANSKY\turan <turan.zoltan0614@gmail.com>
2025-01-20 15:33:56 +0100

    Login Page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,14 @@
 import NavigationCard from "./NavigationCard";
 
-export default function Layout({ children }: any) {
+export default function Layout({ children, hideNavigation }: any) {
     return (
         <div className="flex mt-4 max-w-4xl mx-auto gap-6">
-            <div className="w-3/12">
-                <NavigationCard />
-            </div>
-            <div className="w-9/12">
+            {!hideNavigation && (
+                <div className="w-3/12">
+                    <NavigationCard />
+                </div>
+            )}
+            <div className={hideNavigation ? "w-full" : "w-9/12"}>
                 {children}
             </div>
         </div>
